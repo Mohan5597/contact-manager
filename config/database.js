114061f23c9a0a12e81bc96903b2c19602c9a4ef
Mohan5597 <mohankumar5597@gmail.com>
2019-08-21 17:14:21 +0530

    contacts-app

--- config/database.js
+++ config/database.js
@@ -2,9 +2,10 @@ const mongoose=require('mongoose')
 
 mongoose.Promise=global.Promise
 
+//'mongodb://localhost:27017/contact-app'
 //mongodb+srv://mohan:Mohan1720@cluster0-5szym.mongodb.net/test?retryWrites=true&w=majority
 //process.env.MONGOLAB_URI
-mongoose.connect( process.env.MONGOLAB_URI||'mongodb://localhost:27017/contact-app', {userNewUrlParser:true})
+mongoose.connect( process.env.MONGOLAB_URI, {userNewUrlParser:true})
         .then(() =>{
             console.log('connected to db')
         })
