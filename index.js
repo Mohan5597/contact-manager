44813f5f63723240cbeab89a1c3d3e8254cae548
Mohan5597 <mohankumar5597@gmail.com>
2019-08-21 17:24:17 +0530

    first

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const router = require('./config/routes')
 const path = require('path')
 
 const port = process.env.PORT || 3001
-app.use(express.static(path.join(__dirname,"client/build")))
-//for heroku routers
-app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname + "/client/build/index.html"))
-})
 
 const app=express()
 //const port=3005
