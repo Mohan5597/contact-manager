754c0422b8bf3c35ccf8149446ac5e590784ca0d
Mohan5597 <mohankumar5597@gmail.com>
2019-10-27 12:57:35 +0530

    edit contact

---
+++ client/src/components/contacts/edit.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import axios from 'axios'
+import ContactForm from './form'
+
+class EditContact extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            contact:{},
+            isLoading: true
+        }
+        this.handleSubmit=this.handleSubmit.bind(this)
+    }
+
+    componentDidMount(){
+        const id=this.props.match.params.id
+        axios.get(`http://localhost:3001/contacts/${id}`,{
+            headers:{'x-auth':localStorage.getItem('userAuth')}
+        })
+             .then(response =>{
+                 const contact=response.data
+                 this.setState(() =>({contact,isLoading: false}))
+             })
+    }
+
+    handleSubmit(formData) {
+        const id = this.props.match.params.id
+        axios.put(`http://localhost:3001/contacts/${id}`,formData,{
+            headers:{'x-auth':localStorage.getItem('userAuth')}
+        })
+        .then(response =>{
+            console.log('adghja',response.data)
+            if ((response.data.message) || (response.data.name==="" )||(response.data.mobile===null)) {
+                alert("name/mobile/category required")
+            } else {
+                  this.props.history.push(`/users/show/${id}`)
+
+            }
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h2>Edit Contact</h2>
+                {
+                     (!this.state.isLoading &&<ContactForm
+                    contact={this.state.contact}
+                    isEdit={true}
+                    handleSubmit={this.handleSubmit}/>)
+                }
+            </div>
+        )
+    }
+}
+export default EditContact
