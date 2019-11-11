c129727477304567cbc82b869a808abe85391a21
Mohan5597 <mohankumar5597@gmail.com>
2019-11-11 20:33:14 +0530

    database

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ mongoose.Promise=global.Promise
 //'mongodb://localhost:27017/contact-app'
 //mongodb+srv://mohan:Mohan1720@cluster0-5szym.mongodb.net/test?retryWrites=true&w=majority
 //process.env.MONGOLAB_URI
-mongoose.connect( 'mongodb://localhost:27017/contact-app'|| process.env.MONGODB_URI, {userNewUrlParser:true})
+mongoose.connect(  process.env.MONGODB_URI, {userNewUrlParser:true})
         .then(() =>{
             console.log('connected to db')
         })
