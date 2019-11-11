44aaca7173287fd4ea76aefc4d2269ae3eb46cae
Mohan5597 <mohankumar5597@gmail.com>
2019-11-11 20:15:18 +0530

    port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use('/', router)
 
 
-const port = process.env.PORT
+const port = process.env.PORT ||3001
 app.use(express.static(path.join(__dirname,"client/build")))
 app.get("*",(req,res)=>{
 res.sendFile(path.join(__dirname + "/client/build/index.html"))
