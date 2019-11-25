1551d507ae1319c4700bd550f05c6c5f45852da8
Mohan5597 <mohankumar5597@gmail.com>
2019-11-25 20:07:44 +0530

    initial commit

---
+++ client/build/precache-manifest.af55d508900d53571e1b33eec06972e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fff82c27ece397f057d4d7fd7b93df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3005d051d94724237c9",
+    "url": "/static/css/main.2656ef97.chunk.css"
+  },
+  {
+    "revision": "8fe7ee668ee38b2b6d42",
+    "url": "/static/js/2.ba17b2e0.chunk.js"
+  },
+  {
+    "revision": "a3005d051d94724237c9",
+    "url": "/static/js/main.7d74fb44.chunk.js"
+  },
+  {
+    "revision": "ef074ee3ebb2144ac6cc",
+    "url": "/static/js/runtime-main.eae6c3fe.js"
+  },
+  {
+    "revision": "7bd30993264e192fd4dafcdda830616a",
+    "url": "/static/media/tes.7bd30993.jpg"
+  }
+]);
